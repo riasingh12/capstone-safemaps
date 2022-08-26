3b23e72a221d1ade47d04e5a045f81c205c8d8fe
riasingh12 <riasingh.rs12@gmail.com>
2022-08-26 13:30:39 +0530

    sus2

--- website/auth.py
+++ website/auth.py
@@ -64,3 +64,47 @@ def sign_up():
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
+
+
+# @auth.profile('/profile')
+# @login_required
+# def profile():
+#     return render_template("profile.html", user=current_user)
+
+
+# @auth.route('/profile/edit', methods=['GET', 'POST'])
+# @login_required
+# def edit_profile():
+#     if request.method == 'POST':
+#         email = request.form.get('email')
+#         first_name = request.form.get('firstName')
+#         password1 = request.form.get('password1')
+#         password2 = request.form.get('password2')
+
+#         user = User.query.filter_by(email=email).first()
+#         if user:
+#             flash('Email already exists.', category='error')
+#         elif len(email) < 4:
+#             flash('Email must be greater than 3 characters.', category='error')
+#         elif len(first_name) < 2:
+#             flash('First name must be greater than 1 character.', category='error')
+#         elif password1 != password2:
+#             flash('Passwords don\'t match.', category='error')
+#         elif len(password1) < 7:
+#             flash('Password must be at least 7 characters.', category='error')
+#         else:
+#             current_user.email = email
+#             current_user.first_name = first_name
+#             current_user.password = generate_password_hash(
+#                 password1, method='sha256')
+#             db.session.commit()
+#             flash('Profile updated!', category='success')
+#             return redirect(url_for('auth.profile'))
+
+#     return render_template("edit_profile.html", user=current_user)
+
+
+# @auth.about('/about')
+# def about():
+#     return render_template("about.html", user=current_user)
+
--- website/views.py
+++ website/views.py
@@ -6,32 +6,20 @@ import json
 
 views = Blueprint('views', __name__)
 
-
-@views.route('/', methods=['GET', 'POST'])
+@views.route('/')
 @login_required
 def home():
-    if request.method == 'POST':
-        note = request.form.get('note')
-
-        if len(note) < 1:
-            flash('Note is too short!', category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note added!', category='success')
-
     return render_template("home.html", user=current_user)
 
 
-@views.route('/delete-note', methods=['POST'])
-def delete_note():
-    note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
+@views.route('/about')
+@login_required
+def about():
+    return render_template("about.html", user=current_user)
 
-    return jsonify({})
+
+@views.route('/profile')
+@login_required
+def profile():
+    return render_template("profile.html", user=current_user)
+
